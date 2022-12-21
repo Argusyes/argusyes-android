e62e9b0b7b98f1f5bce792362ed8a1cd0cdaf0b7
cc13446 <1344613920@qq.com>
2022-12-22 00:35:22 +0800

    search

--- app/src/main/java/com/android/argusyes/fragment/ServerAddFragment.kt
+++ app/src/main/java/com/android/argusyes/fragment/ServerAddFragment.kt
@@ -1,15 +1,11 @@
 package com.android.argusyes.fragment
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.Fragment
 import com.android.argusyes.R
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ServerFragment.newInstance] factory method to
+ * Use the [ServerAddFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ServerFragment : Fragment() {
+class ServerAddFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,12 +30,12 @@ class ServerFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_server, container, false)
-        view.findViewById<ImageButton>(R.id.server_button).setColorFilter(ContextCompat.getColor(requireContext(), R.color.red))
-        view.findViewById<ImageView>(R.id.server_search_view).setColorFilter(ContextCompat.getColor(requireContext(), R.color.grey_50))
-
-        return view
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_server_add, container, false)
     }
 
     companion object {
@@ -49,12 +45,12 @@ class ServerFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ServerFragment.
+         * @return A new instance of fragment ServerAddFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ServerFragment().apply {
+            ServerAddFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
