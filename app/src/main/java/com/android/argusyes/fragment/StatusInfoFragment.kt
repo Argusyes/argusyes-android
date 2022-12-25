39b579ab687c2a9ad1b4a9b9c083614be9294f3a
cc13446 <1344613920@qq.com>
2022-12-25 17:55:41 +0800

    status base

---
+++ app/src/main/java/com/android/argusyes/fragment/StatusInfoFragment.kt
@@ -0,0 +1,19 @@
+package com.android.argusyes.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.argusyes.R
+
+class StatusInfoFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_status_info, container, false)
+    }
+}
